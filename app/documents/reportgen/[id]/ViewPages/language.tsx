513991195b289ec45f29e65681987be0b53ed3bb
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-15 11:15:31 +0530

    New lines

--- app/documents/reportgen/[id]/ViewPages/language.tsx
+++ app/documents/reportgen/[id]/ViewPages/language.tsx
@@ -70,7 +70,7 @@ export function PageToJi(pages: Pages): string {
             }
           );
           console.log(content);
-          content = content.join("\\\\");
+          content = content.join(" \\\\ ");
           content = replaceBracesWithContainers(content);
           const verbatim = `${returnBlankSpace(
             2
