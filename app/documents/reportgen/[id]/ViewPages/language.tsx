540f0fd592647e04f9136754c0d4eb8b461d85d2
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-07-19 19:06:28 +0530

    Trying to return content based on element type

--- app/documents/reportgen/[id]/ViewPages/language.tsx
+++ app/documents/reportgen/[id]/ViewPages/language.tsx
@@ -1,11 +1,36 @@
 import getElementName from "@/app/types/elements";
-import { ElementParentType, ElementType, Pages } from "@/app/types/types";
+import {
+  ElementParentType,
+  ElementType,
+  Pages,
+  PdfElement,
+} from "@/app/types/types";
 
-
+function getElementBasedContent(element: PdfElement) {
+  switch (element.element.type) {
+    case ElementType.TITLE:
+    case ElementType.SUBTITLE:
+    case ElementType.HEADING:
+    case ElementType.AUTHOR:
+    case ElementType.DATE:
+    case ElementType.PARAGRAPHS:
+    case ElementType.CODE:
+    case ElementType.ITEMS:
+    case ElementType.FIGURES:
+    case ElementType.CITATIONS:
+    case ElementType.DIFFERENCES:
+    case ElementType.INVALID:
+      return ``;
+  }
+}
 export function PageToJi(pages: Pages): string {
   const output: string[] = [];
 
   pages.forEach((page, index) => {
+    output.push("\\newpage");
+    page.elements.forEach((element) => {
+      const content = getElementBasedContent(element);
+    });
   });
 
   return output.join("\n");
