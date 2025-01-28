e0dcd34541bc9d8113808029040c5c1f6a400538
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-28 13:12:28 +0530

    Creating a page where you can create reports

--- app/generateWithLLM/page.tsx
+++ app/generateWithLLM/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import ButtonBlue1 from "../Components/Buttons/ButtonBlue1";
 import ButtonWhite2 from "../Components/Buttons/ButtonWhite2";
@@ -10,13 +10,13 @@ import Logo3 from "../Components/Images/Logo3";
 export default function Page() {
   return (
     <div className="h-screen flex flex-col bg-[#040e18]">
-      <div className="h-[10%] bg-[#01162B] flex items-center justify-between px-8">
+      <div className="h-[15%] bg-[#01162B] flex items-center justify-between px-8">
         <Logo3 />
         <Navbar />
       </div>
 
       <div className="flex-1 flex justify-center items-center">
-        <div className="bg-[#141a21] p-8 rounded-lg shadow-lg w-full max-w-4xl">
+        <div className="bg-[#01162B] p-8 rounded-lg shadow-lg w-full max-w-4xl">
           <h2 className="text-3xl font-semibold text-center text-white">
             Create Your Report
           </h2>
@@ -61,7 +61,10 @@ export default function Page() {
 
             {/* Submit Button */}
             <div className="flex justify-center mt-6">
-              <button onClick={() => {}} className="w-full sm:w-auto border px-4 py-2 rounded-2xl text-white">
+              <button
+                onClick={() => {}}
+                className="w-full sm:w-auto border px-4 py-2 rounded-2xl text-white"
+              >
                 Submit report
               </button>
             </div>
