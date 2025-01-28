73d3d3d41b490ccdd8bbfc35270642e8e3ee7d06
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-28 14:20:08 +0530

    Reportgeneration

--- app/Components/Images/Logo2.tsx
+++ app/Components/Images/Logo2.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo2() {
   return (
     <Link href={"/"}>
-      <img className="h-max" src="/images/logo_blue.png"></img>
+      <img className="h-28" src="/images/logo_blue.png"></img>
     </Link>
   );
 }
--- app/generateWithLLM/UserInput.tsx
+++ app/generateWithLLM/UserInput.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Logo3 from "../Components/Images/Logo3";
 import Navbar from "../Components/Navbar";
 import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/navigation";
 
 const GENERATE_REPORT = gql`
   mutation generateReport($prompt: String!) {
@@ -10,21 +11,20 @@ const GENERATE_REPORT = gql`
 `;
 function UserInput() {
   const [getReport, { loading, error }] = useMutation(GENERATE_REPORT);
+  const router = useRouter();
   const [userPrompt, setUserPrompt] = useState("");
   async function retrievePDF() {
     try {
-      const data = await getReport({
-        variables: { texFile: userPrompt },
+      const data: any = await getReport({
+        variables: { prompt: userPrompt },
       });
-      console.log(data);
-      //   setPdfData(`${base64PDF}?time=${Date.now()}`);
+      localStorage.setItem("pages", JSON.parse(data.data.CreateReportWithLLM));
+      router.push("/documents/reportgen/0");
     } catch (e) {
       console.error("Error occured", e);
     }
   }
-  function saveCurrentPage() {
-    // localStorage.setItem("pages", JSON.stringify(pagesClone));
-  }
+  function saveCurrentPage() {}
   return (
     <div className="h-screen flex flex-col bg-[#040e18]">
       <div className="h-[15%] bg-[#01162B] flex items-center justify-between px-8">
@@ -39,48 +39,48 @@ function UserInput() {
           </h2>
 
           {/* Form Container */}
-          <form className="space-y-6">
-            {/* Topic Input */}
-            <div>
-              <label
-                htmlFor="topic"
-                className="block text-sm font-medium text-white"
-              >
-                Report Topic*
-              </label>
-              <input
-                id="topic"
-                onChange={(e) => {
-                  setUserPrompt(e.target.value);
-                }}
-                name="topic"
-                type="text"
-                required
-                placeholder="Enter the report topic"
-                className="mt-2 block w-full px-4 py-2 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#01162B] focus:border-transparent"
-              />
-            </div>
+          {/* Topic Input */}
+          <div>
+            <label
+              htmlFor="topic"
+              className="block text-sm font-medium text-white"
+            >
+              Report Topic*
+            </label>
+            <input
+              id="topic"
+              onChange={(e) => {
+                setUserPrompt(e.target.value);
+              }}
+              name="topic"
+              type="text"
+              required
+              placeholder="Enter the report topic"
+              className="mt-2 block w-full px-4 py-2 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#01162B] focus:border-transparent"
+            />
+          </div>
 
-            {/* Report Content */}
-            <div>
-              <label
-                htmlFor="content"
-                className="block text-sm font-medium text-white"
-              >
-                Report Content (Optional)
-              </label>
-              <textarea
-                id="content"
-                name="content"
-                required
-                placeholder="Enter the details of your report"
-                rows={6}
-                className="mt-2 block w-full px-4 py-2 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#01162B] focus:border-transparent"
-              />
-            </div>
+          {/* Report Content */}
+          <div>
+            <label
+              htmlFor="content"
+              className="block text-sm font-medium text-white"
+            >
+              Report Content (Optional)
+            </label>
+            <textarea
+              id="content"
+              name="content"
+              required
+              placeholder="Enter the details of your report"
+              rows={6}
+              className="mt-2 block w-full px-4 py-2 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#01162B] focus:border-transparent"
+            />
+          </div>
 
-            {/* Submit Button */}
-            <div className="flex justify-center mt-6">
+          {/* Submit Button */}
+          <div className="flex justify-center mt-6">
+            {!loading ? (
               <button
                 onClick={() => {
                   retrievePDF();
@@ -89,8 +89,12 @@ function UserInput() {
               >
                 Submit report
               </button>
-            </div>
-          </form>
+            ) : (
+              <text className="text-white font-bold">
+                Generating... please sit back.
+              </text>
+            )}
+          </div>
         </div>
       </div>
     </div>
