d90b639ee5a914f9c9942a7f5e88bfcf74429da9
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-28 13:09:42 +0530

    Creating a third logo

--- app/Components/Buttons/ButtonWhite2.tsx
+++ app/Components/Buttons/ButtonWhite2.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ButtonWhite2() {
   return (
-    <Link href={"https://www.ilovepdf.com/pdf_to_word"} target="_blank">
+    <Link href={"/generateWithLLM"}>
       <button className="text-[#00162B] text-lg  bg-white rounded-3xl py-3 px-8 font-extrabold">
         Convert File
       </button>
