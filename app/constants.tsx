d2b1780c2c04d7e448217173882c6ebf8720751d
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-29 15:57:05 +0530

    Create docs

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,3 +1,4 @@
+// DEvelopment set to true if your backend is running on localhost
 export const DEVELOPMENT = false;
 export const BACKEND_PORT = `${!DEVELOPMENT ? 5000 : 4000}`;
 export const IP = !DEVELOPMENT ? "reportease.ddns.net" : `localhost`;
