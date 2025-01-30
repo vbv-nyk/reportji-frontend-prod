6a2c85549dc6149da1d7a951bdd008875f3cdd1b
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-01-30 10:41:00 +0530

    Changing logos

--- app/Components/Images/Logo2.tsx
+++ app/Components/Images/Logo2.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo2() {
   return (
     <Link href={"/"}>
-      <img className="h-28" src="/images/logo_blue.png"></img>
+      <img className="h-28" src="images/golden_logo.png"></img>
     </Link>
   );
 }
