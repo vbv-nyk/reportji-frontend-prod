89619ffa82ff72b4da9358f056af7bda2e7c9f5c
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-02-27 18:45:04 +0530

    An ieee template

---
+++ app/documents/templates/ieee.tsx
@@ -0,0 +1,11 @@
+export const IEEE = `
+\\documentclass[conference]{IEEEtran}
+\\usepackage{graphicx, amsmath, amssymb, hyperref}
+\\pagestyle{fancy}
+\\fancyhf{}
+\\fancyhead[L]{IEEE Conference}
+\\fancyfoot[C]{\\thepage}
+\\title{Your IEEE Paper Title}
+\\author{Author Name \\\\ Institution \\\\ Email}
+\\date{}
+`
